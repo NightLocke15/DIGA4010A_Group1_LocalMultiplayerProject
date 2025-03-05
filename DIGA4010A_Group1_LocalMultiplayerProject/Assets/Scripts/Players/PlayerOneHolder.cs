220a0991dd8468ec4ea5cbf6a0e71a0f50bec844
NightLocke15 <christinefrances.holt@gmail.com>
2025-03-05 14:01:36 +0200

    In Game UI Part 2 | Player Health Bar Setup

---
+++ DIGA4010A_Group1_LocalMultiplayerProject/Assets/Scripts/Players/PlayerOneHolder.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerOneHolder : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=YJW3TLmckqk for how to add health bar to players
+
+    [SerializeField] private Slider playerHealth;
+
+    private void Start()
+    {
+        playerHealth.value = 100;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            playerHealth.value -= 5;
+        }
+    }
+}
